6a4481275b57e7f730119bb2e06f6886bba0c84a
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-02-07 11:14:59 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 
-import AddForm from './components/AddForm';
-import SmurfDisplay from './components/SmurfDisplay';
-import axios from 'axios';
+import AddForm from "./components/AddForm";
+import SmurfDisplay from "./components/SmurfDisplay";
+import axios from "axios";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+//
 
 class App extends Component {
   render() {
@@ -15,8 +16,8 @@ class App extends Component {
           <a className="navbar-brand">Smurf Village Database</a>
         </nav>
         <main>
-          <AddForm/>
-          <SmurfDisplay/>
+          <AddForm />
+          <SmurfDisplay />
         </main>
       </div>
     );
